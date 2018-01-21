1b195a5beb9c44cd28f30a99797b59f0d00a0a2c
Hadi-Seersol <hamad.pixider@gmail.com>
2018-01-21 17:10:12 +0500

    search member
    fixed search memeber

--- application/views/members/viralMembers.php
+++ application/views/members/viralMembers.php
@@ -14,7 +14,7 @@
                 <div class="box-header">
                     <h3 class="box-title">Members List </h3>
                     <div class="box-tools">
-                        <form action="<?php echo base_url() ?>userListing" method="POST" id="searchList">
+                        <form action="<?php echo base_url() ?>memberListing" method="POST" id="searchList">
                             <div class="input-group">
                               <input type="text" name="searchText" value="<?php echo $searchText; ?>" class="form-control input-sm pull-right" style="width: 150px;" placeholder="Search"/>
                               <div class="input-group-btn">
